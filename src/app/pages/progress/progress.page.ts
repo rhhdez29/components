67ee4f33d9921aeecd401059f5b21964a45435ce
Dulce <dulce.mauricio@alumno.buap.mx>
2025-09-18 23:34:46 -0600

    .

--- src/app/pages/progress/progress.page.ts
+++ src/app/pages/progress/progress.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopoverInfoComponent } from 'src/app/components/popover-info/popover-info.component';
 
 @Component({
   selector: 'app-progress',
@@ -8,14 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressPage implements OnInit {
 
+<<<<<<< HEAD
   porcentaje: number = 0.05;
 
   constructor() { }
+=======
+  constructor(public popoverController: PopoverController) { }
+>>>>>>> 7faadb7c2501cd2252600e06e81362cbe03b43f4
 
   ngOnInit() {
   }
 
+<<<<<<< HEAD
   rangeChange( event : any ) {
     this.porcentaje = event.detail.value / 100;
+=======
+  async presentPopover(e: Event) {
+    const popover = await this.popoverController.create({
+      component: PopoverInfoComponent,
+      event: e,
+    });
+
+    await popover.present();
+>>>>>>> 7faadb7c2501cd2252600e06e81362cbe03b43f4
   }
 }
