b389f9e426dbbeed2c7bbc7d740a46fa8e505919
Dulce <dulce.mauricio@alumno.buap.mx>
2025-09-18 22:40:24 -0600

    progress corregido

--- src/app/pages/progress/progress.page.ts
+++ src/app/pages/progress/progress.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-progress',
   templateUrl: './progress.page.html',
   styleUrls: ['./progress.page.scss'],
+  standalone: false,
 })
 export class ProgressPage implements OnInit {
 
