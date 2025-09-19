b289c5674dce53baad316b0b7d366096ca3a5962
Diego Roman <diegornp1324@gmail.com>
2025-09-19 00:12:53 -0600

    actualiza

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -82,7 +82,11 @@ const routes: Routes = [
   {
     path: 'progress',
     loadChildren: () => import('./pages/progress/progress.module').then( m => m.ProgressPageModule)
+  },  {
+    path: 'refresher',
+    loadChildren: () => import('./pages/refresher/refresher.module').then( m => m.RefresherPageModule)
   },
+
 
 ];
 
---
+++ src/app/pages/refresher/refresher.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-refresher',
+  templateUrl: './refresher.page.html',
+  styleUrls: ['./refresher.page.scss'],
+  standalone: false,
+})
+export class RefresherPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
