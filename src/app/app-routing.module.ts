70900e9aa48b97e6ab1fb82f3c1452a3b9d7f128
Dulce <dulce.mauricio@alumno.buap.mx>
2025-09-19 02:22:53 -0600

    Search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -83,6 +83,10 @@ const routes: Routes = [
     path: 'progress',
     loadChildren: () => import('./pages/progress/progress.module').then( m => m.ProgressPageModule)
   },
+  {
+    path: 'search',
+    loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)
+  },
 
 ];
 
