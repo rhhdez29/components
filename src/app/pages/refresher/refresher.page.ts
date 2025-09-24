0d03dc75ec1e8f3e4d7509925fc2592b4379abc8
rhhdez29 <rafaelhdez290@gmail.com>
2025-09-23 21:56:00 -0600

    refresher arreglado

--- src/app/pages/refresher/refresher.page.ts
+++ src/app/pages/refresher/refresher.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Item {
+  name: string;
+  unread: boolean;
+}
+
 @Component({
   selector: 'app-refresher',
   templateUrl: './refresher.page.html',
@@ -8,9 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RefresherPage implements OnInit {
 
-  constructor() { }
+  public names = [
+    'Burt Bear',
+    'Charlie Cheetah',
+    'Donald Duck',
+    'Eva Eagle',
+    'Ellie Elephant',
+    'Gino Giraffe',
+    'Isabella Iguana',
+    'Karl Kitten',
+    'Lionel Lion',
+    'Molly Mouse',
+    'Paul Puppy',
+    'Rachel Rabbit',
+    'Ted Turtle',
+  ];
+  public items: Item[] = [];
 
   ngOnInit() {
+    this.addItems(5);
   }
 
+  chooseRandomName() {
+    return this.names[Math.floor(Math.random() * this.names.length)];
+  }
+
+  addItems(count: number, unread = false) {
+    for (let i = 0; i < count; i++) {
+      this.items.unshift({
+        name: this.chooseRandomName(),
+        unread: unread,
+      });
+    }
+  }
+
+  handleRefresh(event: any) {
+    setTimeout(() => {
+      this.addItems(3, true);
+      event.target.complete();
+    }, 2000);
+  }
 }
